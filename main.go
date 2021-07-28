5a87e4dcd2cb31db158f9c99a0e97c7e25c97246
dmaizel <dmaizel@redhat.com>
2021-07-28 20:05:30 +0300

    debug

--- main.go
+++ main.go
@@ -1,7 +1,3 @@
-// Copyright (c) 2019 Intel Corporation
-//
-// SPDX-License-Identifier: Apache-2.0
-
 package main
 
 import (
@@ -10,54 +6,45 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
-    "regexp"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-    v1beta1 "/k8s.io/api/admission/v1beta1"
-	whhttp "github.com/slok/kubewebhook/pkg/http"
-	"github.com/slok/kubewebhook/pkg/log"
-	mctx "github.com/slok/kubewebhook/pkg/webhook/context"
-	mutatingwh "github.com/slok/kubewebhook/pkg/webhook/mutating"
+
+	"github.com/sirupsen/logrus"
+	kwhhttp "github.com/slok/kubewebhook/v2/pkg/http"
+	kwhlogrus "github.com/slok/kubewebhook/v2/pkg/log/logrus"
+	kwhmodel "github.com/slok/kubewebhook/v2/pkg/model"
+	kwhmutating "github.com/slok/kubewebhook/v2/pkg/webhook/mutating"
 )
 
-func annotatePodMutator(ctx context.Context, obj metav1.Object) (bool, error) {
+func annotatePodMutator(_ context.Context, ar *kwhmodel.AdmissionReview, obj metav1.Object) (*kwhmutating.MutatorResult, error) {
 	pod, ok := obj.(*corev1.Pod)
 	if !ok {
-		// If not a pod just continue the mutation chain (if there is one) and don't do anything
-		return false, nil
+		// If not a pod just continue the mutation chain(if there is one) and don't do nothing.
+		return &kwhmutating.MutatorResult{}, nil
 	}
-
-	request := mctx.GetAdmissionRequest(ctx)
-	if request == nil {
-		return false, nil
-	}
-
-	// The Namespace is not always available in the pod Spec
-	// specially when operators create the pods. Hence access
-	// the Namespace in the actual request (vs the object)
-	// https://godoc.org/k8s.io/api/admission/v1beta1#AdmissionRequest
 
 	for k, v := range whPolicy.nsBlacklist {
-        fmt.Printf("key[%s] value[%s]\n", k, v)
-		match, _ := regexp.MatchString(k, request.Namespace)
+		fmt.Printf("key[%s] value[%s]\n", k, v)
+		match, _ := regexp.MatchString(k, ar.Namespace)
 
-        if match {
-            fmt.Println("blacklisted namespace: ", request.Namespace)
-            return false, nil
-        }
-    }
-
-	if whPolicy.nsBlacklist[request.Namespace] {
-		fmt.Println("blacklisted namespace: ", request.Namespace)
-		return false, nil
+		if match {
+			fmt.Println("blacklisted namespace: ", ar.Namespace)
+		    return &kwhmutating.MutatorResult{}, nil
+		}
 	}
 
-    if validRegex != nil {
-		if !validRegex.MatchString(request.Namespace) {
-			fmt.Println("namespace doesn't match regex expression", request.Namespace)
-			return false, nil
+	if whPolicy.nsBlacklist[ar.Namespace] {
+		fmt.Println("blacklisted namespace: ", ar.Namespace)
+		return &kwhmutating.MutatorResult{}, nil
+	}
+
+	if validRegex != nil {
+		if !validRegex.MatchString(ar.Namespace) {
+			fmt.Println("namespace doesn't match regex expression", ar.Namespace)
+		    return &kwhmutating.MutatorResult{}, nil
 		}
 	}
 
@@ -65,40 +52,32 @@ func annotatePodMutator(ctx context.Context, obj metav1.Object) (bool, error) {
 	// https://github.com/kata-containers/documentation/blob/master/Limitations.md#docker---nethost
 	if pod.Spec.HostNetwork {
 		fmt.Println("host network: ", pod.GetNamespace(), pod.GetName())
-		return false, nil
+        return &kwhmutating.MutatorResult{}, nil
 	}
 
 	for i := range pod.Spec.Containers {
 		if pod.Spec.Containers[i].SecurityContext != nil && pod.Spec.Containers[i].SecurityContext.Privileged != nil {
 			if *pod.Spec.Containers[i].SecurityContext.Privileged {
 				fmt.Println("privileged container: ", pod.GetNamespace(), pod.GetName())
-				return false, nil
+		        return &kwhmutating.MutatorResult{}, nil
 			}
 		}
 	}
 
 	if pod.Spec.RuntimeClassName != nil {
 		fmt.Println("explicit runtime: ", pod.GetNamespace(), pod.GetName(), pod.Spec.RuntimeClassName)
-		return false, nil
+        return &kwhmutating.MutatorResult{}, nil
 	}
 
 	// Mutate the pod
 	fmt.Println("setting runtime to kata: ", pod.GetNamespace(), pod.GetName())
 
-    // Create a response to return to the Kubernetes API
-    ar := v1beta1.AdmissionReview{
-      Response: &v1beta1.AdmissionResponse{
-        Allowed: true,
-//         Result: &metav1.Status{
-//           Message: "Mutated successfully!",
-//         },
-      },
-    }
-
 	kataRuntimeClassName := "kata"
 	pod.Spec.RuntimeClassName = &kataRuntimeClassName
 
-	return false, nil
+	return &kwhmutating.MutatorResult{
+		MutatedObject: pod,
+	}, nil
 }
 
 type config struct {
@@ -122,14 +101,16 @@ func initFlags() *config {
 	fl.StringVar(&cfg.certFile, "tls-cert-file", "", "TLS certificate file")
 	fl.StringVar(&cfg.keyFile, "tls-key-file", "", "TLS key file")
 	fl.StringVar(&cfg.nsBlacklist, "exclude-regex-namespaces", "", "Comma separated namespace blacklist")
-    fl.StringVar(&cfg.regexStr, "regex-matching-namespaces", "", "regex string, will mutate only on matching namespaces, blacklisted namespaces will be ingnored")
+	fl.StringVar(&cfg.regexStr, "regex-matching-namespaces", "", "regex string, will mutate only on matching namespaces, blacklisted namespaces will be ingnored")
 
 	fl.Parse(os.Args[1:])
 	return cfg
 }
 
 func main() {
-	logger := &log.Std{Debug: true}
+	logrusLogEntry := logrus.NewEntry(logrus.New())
+	logrusLogEntry.Logger.SetLevel(logrus.DebugLevel)
+	logger := kwhlogrus.NewLogrus(logrusLogEntry)
 
 	cfg := initFlags()
 
@@ -147,28 +128,28 @@ func main() {
 	}
 
 	// Create our mutator
-	mt := mutatingwh.MutatorFunc(annotatePodMutator)
+	mt := kwhmutating.MutatorFunc(annotatePodMutator)
 
-	mcfg := mutatingwh.WebhookConfig{
-		Name: "podAnnotate",
-		Obj:  &corev1.Pod{},
+	mcfg := kwhmutating.WebhookConfig{
+		ID:      "podAnnotate",
+		Obj:     &corev1.Pod{},
+		Mutator: mt,
+		Logger:  logger,
 	}
-	wh, err := mutatingwh.NewWebhook(mcfg, mt, nil, nil, logger)
+	wh, err := kwhmutating.NewWebhook(mcfg)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error creating webhook: %s", err)
 		os.Exit(1)
 	}
 
 	// Get the handler for our webhook.
-	whHandler, err := whhttp.HandlerFor(wh)
+	whHandler, err := kwhhttp.HandlerFor(kwhhttp.HandlerConfig{Webhook: wh, Logger: logger})
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error creating webhook handler: %s", err)
 		os.Exit(1)
 	}
-
-	port := ":8080"
-	logger.Infof("Listening on %s", port)
-	err = http.ListenAndServeTLS(port, cfg.certFile, cfg.keyFile, whHandler)
+	logger.Infof("Listening on :8080")
+	err = http.ListenAndServeTLS(":8080", cfg.certFile, cfg.keyFile, whHandler)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error serving webhook: %s", err)
 		os.Exit(1)
