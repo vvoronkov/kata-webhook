a444223dbe3a07d620e15ef55f0b625592958429
Victor Voronkov <vvoronkov@users.noreply.github.com>
2022-03-24 11:03:31 +0200

    added extensions file to provide valid form of SAN

--- create-certs.sh
+++ create-certs.sh
@@ -9,18 +9,14 @@ WEBHOOK_NAME=${2:-"pod-annotate"}
 WEBHOOK_SVC="${WEBHOOK_NAME}-webhook"
 
 # Create CA for signing webhook cert
-openssl genrsa -out webhookCA.key 2048
-openssl req -x509 -new -nodes -key webhookCA.key \
-    -subj "/CN=${WEBHOOK_SVC}.${WEBHOOK_NS}.svc.ca" \
-    -addext "subjectAltName = DNS:${WEBHOOK_SVC}.${WEBHOOK_NS}.svc.ca" \
-    -days 365 -out webhookCA.crt
+openssl genrsa -out ca.key 2048
+openssl req -x509 -new -nodes -key ca.key -subj "/CN=${WEBHOOK_SVC}.${WEBHOOK_NS}.ca" -days 365 -out ca.crt
 
 # Create certs for our webhook
-openssl genrsa -out webhook.key 2048
-openssl req -new -key ./webhook.key \
-    -subj "/CN=${WEBHOOK_SVC}.${WEBHOOK_NS}.svc" \
-    -addext "subjectAltName = DNS:${WEBHOOK_SVC}.${WEBHOOK_NS}.svc" -out ./webhook.csr
-openssl x509 -req -days 365 -in webhook.csr -CA webhookCA.crt -CAkey webhookCA.key -CAcreateserial -out webhook.crt
+openssl req -newkey rsa:2048 -nodes -keyout webhook.key -subj "/CN=${WEBHOOK_SVC}.${WEBHOOK_NS}.svc" -out ./webhook.csr
+echo "subjectAltName=DNS:${WEBHOOK_SVC}.${WEBHOOK_NS}.svc" > extensions.txt
+echo "extendedKeyUsage=serverAuth" >> extensions.txt
+openssl x509 -req -extfile extensions.txt -days 365 -in webhook.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out webhook.crt
 
 # Create certs secrets for k8s
 kubectl create -n ${WEBHOOK_NS} secret generic \
@@ -30,7 +26,7 @@ kubectl create -n ${WEBHOOK_NS} secret generic \
     --dry-run -o yaml > ./deploy/webhook-certs.yaml
 
 # Set the CABundle on the webhook registration
-CA_BUNDLE=$(cat webhookCA.crt | base64 -w0)
+CA_BUNDLE=$(cat ca.crt | base64 -w0)
 sed -e "s/PROJECT_NAMESPACE/${WEBHOOK_NS}/" -e "s/CA_BUNDLE/${CA_BUNDLE}/" ./deploy/webhook-registration.yaml.tpl > ./deploy/webhook-registration.yaml
 
 # Clean
